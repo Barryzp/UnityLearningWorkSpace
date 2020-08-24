8af91506a9485906937046bbde15122295484fac
Barryzp <yihengzhang001@163.com>
2020-08-24 14:31:58 +0800

    Merge changes,

--- BasicFSM/Assets/Scripts/FSM/FSMSystem.cs
+++ BasicFSM/Assets/Scripts/FSM/FSMSystem.cs
@@ -52,6 +52,10 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 public enum Transition
 {
     NullTransition = 0, // Use this transition to represent a non-existing transition in your system
+    SawPlayer,
+    ReachPlayer,
+    LostPlayer,
+    NoHealth
 }
 
 /// <summary>
@@ -61,6 +65,10 @@ public enum Transition
 public enum StateID
 {
     NullStateID = 0, // Use this ID to represent a non-existing State in your system	
+    Patrolling,
+    Chasing,
+    Attacking,
+    Dead
 }
 
 /// <summary>
@@ -159,14 +167,14 @@ public abstract class FSMState
     /// This method decides if the state should transition to another on its list
     /// NPC is a reference to the object that is controlled by this class
     /// </summary>
-    public abstract void Reason(GameObject player, GameObject npc);
+    public abstract void Reason(Transform player, Transform npc);
 
     /// <summary>
     /// This method controls the behavior of the NPC in the game World.
     /// Every action, movement or communication the NPC does should be placed here
     /// NPC is a reference to the object that is controlled by this class
     /// </summary>
-    public abstract void Act(GameObject player, GameObject npc);
+    public abstract void Act(Transform player, Transform npc);
 
 } // class FSMState
 
---
+++ BasicFSM/Assets/Scripts/FSM/PatrolState.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolState :FSMState
+{
+    private Transform[] wayPoints;
+    private float curRotSpeed = 0.1f;
+    private float curSpeed = 100.0f;
+    private Vector3 destPos = Vector3.zero;
+
+    private void FindNextPoint(){
+        print("find next point.");
+    }
+
+    public PatrolState(Transform[] wanderPoints){
+        wayPoints = wanderPoints;
+        curRotSpeed = 0.3f;
+        curSpeed = 3;
+    }
+
+    public override void Reason(Transform player, Transform npc)
+    {
+        var distanceFromPlayer = Vector3.Distance(npc.position,player.position);
+        var distanceFromWanderP = Vector3.Distance(npc.position,destPos);
+
+        if(distanceFromPlayer <= 1){
+
+        }
+    }
+
+    public override void Act(Transform player, Transform npc)
+    {
+    }
+}
